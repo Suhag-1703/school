eab44bde80355da0282bbb70e6c21a9000828227
Deepti Suhag <suhagdeepti95@gmail.com>
2025-06-09 00:39:06 +0530

    add saas and primereact

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
+
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+
+        
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+  <PrimeReactProvider value={{ unstyled: true }}>
+
     <App />
+  </PrimeReactProvider>
   </React.StrictMode>
 );
 
