1503f3274c676b2477329cf724074fcc44800051
Suhag-1703 <suhagdeepti95@gmail.com>
2025-06-10 12:25:09 +0530

    Create Loader.tsx

---
+++ src/components/Loader.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Loader: React.FC = () => {
+
+  return (
+    <>
+      <div className="overlay">
+        <span className="loader"></span>
+      </div>
+    </>
+  )
+}
+
+export default Loader;
